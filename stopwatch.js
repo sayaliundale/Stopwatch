cae029a69bf38600beeb4eebdce9720d650ec889
Sayali <sayali.undale_comp21@pccoer.in>
2023-09-16 13:38:06 +0530

    Audio added with pause functionality

--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,6 @@
 let i = null; 
+let Audio1=null;
+let volume=1.0;
 
 function set() {
   var input = document.getElementById("input").value;
@@ -8,28 +10,54 @@ function set() {
   let timer = parseInt(input);
   let timer1 = parseInt(input1);
   let timer2 = parseInt(input2);
-
-  function counter() {
-    if (timer >= 1) {
-      timer--;
-
-      if (timer === 0) {
-        if (timer1 >= 1) {
-          timer1--;
-          timer = 59;
-        }
-
-        if (timer1 === 0 && timer2 >= 1) {
-          timer2--;
-          timer1 = 59;
-        }
+ 
+  if(isNaN(timer2)){
+    timer2=0
+  }
+  if(isNaN(timer1)){
+    timer1=0
+  }
+  if(isNaN(timer)){
+    timer=0
+  }
+ 
+ Audio1 = document.getElementById("audio");
+  
+  function counter() 
+  {
+    if(timer===0 && timer1===0 && timer2===0){
+      document.getElementById("stop").style.visibility="hidden";
+      document.getElementById("resume").style.visibility="hidden";
+      document.getElementById("set").style.visibility="hidden";
+      document.getElementById("pause").style.display="block";
+     Audio1.play();
+     volume=Audio1.volume;
+    }
+    
+    
+    if(timer === 0) {
+      
+      if(timer1 > 0) 
+      {
+        timer1--;
+        timer = 59;
+        
+      } 
+      else if(timer2 > 0)
+      {
+        timer2--;
+        timer1 = 59;
       }
+    } 
+    else{
+      timer--;
+    }
 
       document.getElementById("hours").innerHTML = timer2;
       document.getElementById("minutes").innerHTML = timer1;
       document.getElementById("seconds").innerHTML = timer;
     }
-  }
+  
 
   i = setInterval(counter, 1000);
 }
@@ -37,3 +65,18 @@ function set() {
 function stop() {
   clearInterval(i); 
 }
+function audiop() {
+  if (Audio1) { 
+    Audio1.pause();
+    Audio1.volume=0;
+  
+  }
+}
+audiop();
+
+
+
+
+
+
+
